3078ad9dbc0affd6bfeda844ab4b6dba316609a3
KenjiMorita <kennjimoritata@yahoo.co.jp>
2015-09-05 07:56:29 +0900

    create extra.js because learn service

--- src/scripts/controllers/extra/extra.js
+++ src/scripts/controllers/extra/extra.js
@@ -1,47 +1,65 @@
 var myApp = angular.module('myApp', []);
-myApp.value("myData",
-		{"data" : [
-					{ "namae": "kenji", "age": 30, "status" : "yes", "living": "kuki" },
-					{ "namae": "kenji2", "age": 20, "status" : "yes", "living": "kawasaki" },
-					{ "namae": "kenj3", "age": 30, "status" : "no", "living": "megurdadao" },
-					{ "namae": "kenji4", "age": 20, "status" : "yes", "living": "kawasaki" },
-					{ "namae": "kenji5", "age": 30, "status" : "no", "living": "meguro" },
-					{ "namae": "kenji6", "age": 20, "status" : "yes", "living": "kawasaki" },
-					{ "namae": "kenji7", "age": 30, "status" : "no", "living": "kuki" },
-					{ "namae": "kenji8", "age": 20, "status" : "yes", "living": "kawasaki" },
-					{ "namae": "kenji9", "age": 40, "status" : "yes", "living": "meguro" }
-				]
-	})
-	.value('AppTitle','AngularJSPrograming')
-	.value('AppInfo',
-		{
-			 autor : '森田',
-			 update : new Date(2007,5,25)
-		})
-.value('CommonProc',function(){
- console.log(value)
+myApp.value("myData", {
+  "data": [{
+    "namae": "kenji",
+    "age": 30,
+    "status": "yes",
+    "living": "kuki"
+  }, {
+    "namae": "kenji2",
+    "age": 20,
+    "status": "yes",
+    "living": "kawasaki"
+  }, {
+    "namae": "kenj3",
+    "age": 30,
+    "status": "no",
+    "living": "megurdadao"
+  }, {
+    "namae": "kenji4",
+    "age": 20,
+    "status": "yes",
+    "living": "kawasaki"
+  }, {
+    "namae": "kenji5",
+    "age": 30,
+    "status": "no",
+    "living": "meguro"
+  }, {
+    "namae": "kenji6",
+    "age": 20,
+    "status": "yes",
+    "living": "kawasaki"
+  }, {
+    "namae": "kenji7",
+    "age": 30,
+    "status": "no",
+    "living": "kuki"
+  }, {
+    "namae": "kenji8",
+    "age": 20,
+    "status": "yes",
+    "living": "kawasaki"
+  }, {
+    "namae": "kenji9",
+    "age": 40,
+    "status": "yes",
+    "living": "meguro"
+  }]
 });
-myApp.service('filterDateService',['myData',function(myData){
-	  console.log(myData);
-		this.getMydata = function()
-		{
-			return myData;
-		}
+myApp.service('filterDateService', ['myData', function(myData) {
+  console.log(myData);
+  this.getMydata = function() {
+    return myData;
+  }
 }]);
-myApp.factory('filterDataFactory',['myData',function(myData){
-	return {
-		getMydata : function()
-		{
-			return myData;
-		}
+myApp.filter('myFilter',function(){
+	if(this.checked){
+		return data.status.match(/yes/);
 	}
-}]);
-myApp.controller('SampleController',['AppTitle','AppInfo','CommonProc','filterDateService','filterDataFactory',
-function(AppTitle, AppInfo , CommonProc,filterDateService,filterDataFactory){
-		console.log(filterDataFactory.getMyData);
-		this.AppTitle = AppTitle;
-		this.getedFactory = filterDataFactory.getMydata();
-		this.getedService = filterDateService.getMydata();
-		this.AppInfo = AppInfo;
-		this.CommonProc = CommonProc;
-}]);
+})
+myApp.controller('SampleController', ['filterDateService',
+  function(filterDateService) {
+    this.getedService = filterDateService.getMydata();
+  }
+]);
