b983b64326e0bab3e2c7982d51de8367c14338cb
KenjiMorita <kennjimoritata@yahoo.co.jp>
2015-10-31 07:20:10 +0900

    fix jade

--- src/scripts/controllers/controller.ts
+++ src/scripts/controllers/controller.ts
@@ -1,14 +1,30 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
 "use strict";
-import eee from 'test'
+// import eee from 'test'
 
 class MyController{
-		public name:string;
-		public age:string;
-		public morita: string;
-		public ffa: string;
+    constructor(public value){
+        this.value = value;
+    }
+    public name:string;
+    public age:string;
+    public morita: string;
+    public ffa: string;
 }
 
 var  tabibito = angular.module('tabibito',[]);
-tabibito.controller('myController',MyController);
+tabibito.value('value',function(){
+    return [
+       {
+           name : 'kenji',
+           age : 92
+       },
+       {
+           name : 'kenji',
+           age : 92
+       }
+
+    ]
+})
+tabibito.controller('myController', function() { new MyController(); });
